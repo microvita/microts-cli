32d0e57ee578fec52c00ed2b0bbcb6489f380605
micro-vita <microlinux2024@gmail.com>
2024-04-20 17:44:12 +0630

    Release Version

--- bin/lib/ui/messages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MESSAGES = void 0;
-exports.MESSAGES = {
-    RUNNER_EXECUTION_ERROR: (command) => `\nFailed to execute command: ${command}`,
-};
-//# sourceMappingURL=messages.js.map
